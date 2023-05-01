9128cb022249db66895b5cecaf137d5b3bf2333d
Paahxp123 <joaopedro2k2294@gmail.com>
2023-04-30 21:04:10 -0300

    Criação das rotas Get e Delet dos model Serviços

--- routes/servicos.js
+++ routes/servicos.js
@@ -4,23 +4,66 @@ const Servico = require("../database/servico");
 // Criar o grupo de rotas (/clientes)
 const router = Router();
 
-
 router.post("/servicos", async (req, res) => {
+  const { nome, preco } = req.body;
 
-    const { nome, preco } = req.body;
-
-    try {
-        if (nome, preco) {
-            const novoServico = await Servico.create({ nome, preco });
-            res.status(201).json(novoServico);
-        } else {
-            res.json({ message: "Nome ou preco invalido" });
-        }
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ message: "Acnteceu um erro." });
+  try {
+    if ((nome, preco)) {
+      const novoServico = await Servico.create({ nome, preco });
+      res.status(201).json(novoServico);
+    } else {
+      res.json({ message: "Nome ou preco invalido" });
     }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Aconteceu um erro." });
+  }
+});
 
+router.get("/servicos", async (req, res) => {
+  const Servicos = await Servico.findAll();
+  res.json(Servicos);
+});
+
+router.get("/servicos/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const servico = await Servico.findByPk(id);
+    if (servico) {
+      res.json(servico);
+    } else {
+      res.status(404).json({ message: "Serviço não encontrado." });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Ocorreu um erro." });
+  }
+});
+
+router.delete("/servicos/:id", async (req, res) => {
+  try {
+    const servico = await Servico.findByPk(req.params.id);
+    if (servico) {
+      await servico.destroy();
+      res.json({ message: "Serviço foi removido." });
+    } else {
+      res.status(404).json({ message: "Serviço não encontrado." });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Um erro aconteceu." });
+  }
+});
+
+router.delete("/servicos-all", async (req, res) => {
+  try {
+    await Servico.destroy({ truncate: true });
+    res.json({ message: "Todos os servicos foram removidos." });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Um erro aconteceu." });
+  }
 });
 
 module.exports = router;
