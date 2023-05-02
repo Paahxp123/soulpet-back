d79eb4152337b97b42b6792acf0f9c5ef5d882e0
lauraxavier <laura.ac.xavier@gmail.com>
2023-05-02 15:43:13 -0300

    Recurso Get para listagem de agendamentos

--- routes/agendamentos.js
+++ routes/agendamentos.js
@@ -3,6 +3,7 @@ const Agendamento = require("../database/agendamento");
 
 const router = Router();
 
+// Inserir agendamento
 router.post("/agendamentos", async (req, res) => {
   const { dataAgendada, realizada, petId, servicoId } = req.body;
   try {
@@ -10,6 +11,17 @@ router.post("/agendamentos", async (req, res) => {
       { dataAgendada, realizada, petId, servicoId },
     );
     res.status(201).json(novoAgendamento);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Um erro aconteceu." });
+  }
+});
+
+// Listar agendamentos
+router.get("/agendamentos", async (req, res) => {
+  try {
+    const agendamentos = await Agendamento.findAll();
+    res.status(200).json(agendamentos);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Um erro aconteceu." });
